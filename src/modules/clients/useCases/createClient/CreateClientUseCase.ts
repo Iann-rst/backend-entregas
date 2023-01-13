77d78c1ba0782355bacdd08133b58d6a67642199
Iann-rst <iannrst11@gmail.com>
2023-01-13 15:24:54 -0300

    feat: UseCase de criação de Client

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from 'bcrypt'
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ password, username }: ICreateClient) {
+    //Validar se o usuário existe (se existir retorna um erro)
+    const clientExist = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive"
+        }
+      }
+    })
+
+    if (clientExist) {
+      throw new Error("Client Already exists")
+    }
+    //Criptografar a senha
+    const hashPassword = await hash(password, 10)
+
+    //Salvar o client
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    })
+
+    return client
+  }
+}
