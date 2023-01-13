153a2bb87b5461d3d02f71e4ae2c6a17c9ba97b4
Iann-rst <iannrst11@gmail.com>
2023-01-13 15:56:44 -0300

    feat: rota para autenticação de client

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
+import { AuthenticateClientController } from './modules/accounts/useCases/authenticateClient/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { Router } from "express";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
-routes.post("/client/", createClientController.handle)
+const authenticateClientController = new AuthenticateClientController();
+
+
+routes.post("/authenticate", authenticateClientController.handle)
+routes.post("/client/", createClientController.handle);
+
 
 export { routes };
