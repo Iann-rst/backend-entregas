975ee591611a1bb7884d2ecb9e4361e61c6a1c37
Iann-rst <iannrst11@gmail.com>
2023-01-13 13:12:36 -0300

    refactor: adicionando id como primary key

---
+++ prisma/migrations/20230113161206_refactor_id_primay_key/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "clients" ADD CONSTRAINT "clients_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "deliveryman" ADD CONSTRAINT "deliveryman_pkey" PRIMARY KEY ("id");
