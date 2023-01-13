cabf6f3455b5547c611b58fd40e3fbf0e9a59f62
Iann-rst <iannrst11@gmail.com>
2023-01-13 16:02:10 -0300

    feat: tratamento de exceções

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import express from "express"
+import express, { NextFunction, Response, Request } from "express"
+import "express-async-errors";
+
 import { routes } from "./routes";
 
 const app = express();
@@ -6,9 +8,16 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Projeto configurado"
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
   })
 })
 app.listen(3000, () => console.log("Server is running"))
