918b92203093695c29f5e0f7733ce461d2360627
Iann-rst <iannrst11@gmail.com>
2023-01-13 16:58:53 -0300

    feat: rota para cadastrar um deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 import { AuthenticateClientController } from './modules/accounts/useCases/authenticateClient/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { Router } from "express";
@@ -7,9 +8,13 @@ const routes = Router();
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 
+const createDeliverymanController = new CreateDeliverymanController();
 
+//Clients
 routes.post("/authenticate", authenticateClientController.handle)
 routes.post("/client/", createClientController.handle);
 
+//Deliveryman
+routes.post("/deliveryman/", createDeliverymanController.handle)
 
 export { routes };
