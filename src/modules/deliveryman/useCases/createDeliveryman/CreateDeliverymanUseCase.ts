edaf1a963489a8616627288ed2d920192c642812
Iann-rst <iannrst11@gmail.com>
2023-01-13 16:59:06 -0300

    feat: UseCase para cadastrar deliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,36 @@
+import { hash } from 'bcrypt';
+import { prisma } from '../../../../database/prismaClient'
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class CreateDeliverymanUseCase {
+  async execute({ username, password }: ICreateDeliveryman) {
+
+    const deliverymanExist = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          mode: "insensitive"
+        }
+      }
+    })
+
+    if (deliverymanExist) {
+      throw new Error("Deliveryman Already exists")
+    }
+
+    const hashPassword = await hash(password, 10)
+
+
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    })
+
+    return deliveryman;
+  }
+}
