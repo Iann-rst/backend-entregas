b73e651f0260c32d7eeac2c4d02b40e4c38dd5aa
Iann-rst <iannrst11@gmail.com>
2023-01-13 17:20:04 -0300

    feat: UseCase para autenticação do deliveryman

---
+++ src/modules/accounts/useCases/authenticateDeliveryman/AuthenticateDeliverymanController.ts
@@ -0,0 +1,14 @@
+import { AuthenticateDeliverymanUseCase } from './AuthenticateDeliveryUseCase';
+import { Request, Response } from "express";
+
+export class AuthenticateDeliverymanController {
+  async handle(request: Request, response: Response) {
+    const { username, password } = request.body;
+
+    const authenticateDeliverymanUseCase = new AuthenticateDeliverymanUseCase();
+
+    const result = await authenticateDeliverymanUseCase.execute({ username, password });
+
+    return response.json(result);
+  }
+}
