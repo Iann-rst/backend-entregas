2dfca368ece5370bd32b8eef55d66ed68ee1006b
Iann-rst <iannrst11@gmail.com>
2023-01-13 17:19:49 -0300

    feat: Rota para autenticação do deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { CreateDeliverymanController } from './modules/deliveryman/useCases/crea
 import { AuthenticateClientController } from './modules/accounts/useCases/authenticateClient/AuthenticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 import { Router } from "express";
+import { AuthenticateDeliverymanController } from './modules/accounts/useCases/authenticateDeliveryman/AuthenticateDeliverymanController';
 
 const routes = Router();
 
@@ -9,12 +10,14 @@ const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 
 const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 
 //Clients
-routes.post("/authenticate", authenticateClientController.handle)
+routes.post("/client/authenticate/", authenticateClientController.handle)
 routes.post("/client/", createClientController.handle);
 
 //Deliveryman
-routes.post("/deliveryman/", createDeliverymanController.handle)
+routes.post("/deliveryman/", createDeliverymanController.handle);
+routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle);
 
 export { routes };
