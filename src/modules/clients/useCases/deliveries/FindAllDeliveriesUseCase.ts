da79f3c7b182a0857fce6517ef53386c170a500c
Iann-rst <iannrst11@gmail.com>
2023-01-13 20:06:52 -0300

    feat: UseCase para buscar as deliveries do cliente

---
+++ src/modules/clients/useCases/deliveries/FindAllDeliveriesUseCase.ts
@@ -0,0 +1,18 @@
+import { prisma } from "../../../../database/prismaClient";
+
+export class FindAllDeliveriesUseCase {
+  async execute(id_client: string) {
+    const deliveries = await prisma.clients.findMany({
+      where: {
+        id: id_client
+      },
+      select: {
+        username: true,
+        id: true,
+        deliveries: true,
+      }
+    })
+
+    return deliveries;
+  }
+}
